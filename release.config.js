9249ce3231380f133e9480097e7ab45f7c329982
Luis-Rosario-Alers <luisrosarioalers@gmail.com>
2024-12-03 12:45:27 -0600

    chore: trying to get actions to work

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   branches: ['main'],
+  repositoryUrl: 'https://github.com/Luis-Rosario-Alers/CarbonCalculator',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
