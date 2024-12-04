3675cad5dd2ef84428948247b028b29bcda3fd02
Luis-Rosario-Alers <luisrosarioalers@gmail.com>
2024-12-04 12:31:10 -0600

    chore: removed the trash sem release js and fixed file paths

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.core.emissions_calculator import EmissionsCalculator
-from src.core.input_handler import input_handler
+from src.core.input_handler import inputhandler
 from src.data.database import database_initialization
 
 # introduction phase
@@ -11,7 +11,7 @@ database_initialization()
 
 # working phase
 emissions_calculator = EmissionsCalculator()
-userinput = input_handler.get_user_input()
+userinput = inputhandler.get_user_input()
 result = emissions_calculator.calculate_emissions(
     userinput[0], userinput[1], userinput[2]
 )
