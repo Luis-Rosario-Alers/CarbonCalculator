b050a661902beeef4f8ab6e9270a99cfea0ddabc
Luis Rosario Alers <luisrosarioalers@gmail.com>
2024-12-17 19:23:10 -0600

    chore: changed setup.py version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="CarbonCalculator",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
